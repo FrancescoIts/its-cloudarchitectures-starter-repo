eeab846731d900d4f1852631269d50e1ae1f7a83
Francies <franciesdev@gmail.com>
2026-03-27 17:46:56 +0100

    /health

--- app/server.js
+++ app/server.js
@@ -5,8 +5,8 @@ app.get("/", (req, res) => {
   res.send("Hello from Node.js inside Docker! 🚀");
 });
 
-/*app.get("/health", (req, res) => {
+app.get("/health", (req, res) => {
   res.json({ status: "ok", timestamp: Date.now() });
-});*/
+});
 
 app.listen(3000, () => console.log("Server running on port 3000"));
